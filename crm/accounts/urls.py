117952c940cc263efe32e6c7c9101a3dc37c67b2
vr3n <vickyspatel@gmail.com>
2020-10-10 01:14:30 +0530

    Orders can be placed for specific customer instance

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('contact/', contact, name="contact"),
     path('products/', products, name="products"),
     path('customer/<int:pk>/', customers, name="customer"),
-    path('create_order/', createOrder, name="create_order"),
+    path('create_order/<int:pk>/', createOrder, name="create_order"),
     path('update_order/<int:pk>/', updateOrder, name="update_order"),
     path('delete_order/<int:pk>/', deleteOrder, name="delete_order"),
 ]
