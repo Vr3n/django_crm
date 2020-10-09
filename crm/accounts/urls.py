fa6b51dd8f9f2a6291b3e63eedf774b9aa5246ee
vr3n <vickyspatel@gmail.com>
2020-10-10 00:52:41 +0530

    Created CRUD for ORDERS

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home, contact, products, customers
+from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
     path('contact/', contact, name="contact"),
     path('products/', products, name="products"),
-    path('customer/<int:pk>/', customers, name="customers"),
+    path('customer/<int:pk>/', customers, name="customer"),
+    path('create_order/', createOrder, name="create_order"),
+    path('update_order/<int:pk>/', updateOrder, name="update_order"),
+    path('delete_order/<int:pk>/', deleteOrder, name="delete_order"),
 ]
