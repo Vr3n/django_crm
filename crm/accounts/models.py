f29392eee45650615988a89e31266d72bd85edc6
vr3n <vickyspatel@gmail.com>
2020-10-09 22:31:53 +0530

    Added template tags and queries

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.IntegerField(null=True)
+    email = models.EmailField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Graphic Card', 'Graphic Card'),
+        ('Processor', 'Processor'),
+        ('RAM', 'RAM'),
+        ('Monitors', 'Monitors'),
+        ('Camera', 'Camera'),
+        ('DJ-DECK', 'DJ-DECK'),
+    )
+
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    category = models.CharField(max_length=200, choices=CATEGORY)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def __str__(self):
+        return f'{self.customer} - {self.product} - {self.status}'
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+
+    delivered_orders = orders.filter(status='Delivered').count()
+    pending_orders = orders.filter(status='Pending').count()
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered_orders': delivered_orders,
+        'pending_orders': pending_orders
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def contact(request):
     return render(request, 'accounts/contact.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'accounts/products.html', context)
 
 
 def customers(request):
